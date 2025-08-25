13b040636749fce7a6ca35b77f88bf52b9e9397d
Evan Yang <magicsquirrelgames@gmail.com>
2025-08-24 18:25:49 -0700

    Inital commit

---
+++ MovieList/Source Files/main.c
@@ -0,0 +1,24 @@
+/*
+    Author: Evan Yang
+    Date: 8-22-2025
+    Description: This application will read and parse a .csv file with movie information,
+    allow the user to edit the information and store t back to the .csv file.
+*/
+
+#define _CRT_SEC_NO_WARNINGS
+
+#include <stdio.h> // printf(), scanf(), fprint(), fgets(), fputs(), fscanf()
+#include <string.h> // strcpy(), strcat(), strcmp(), strlen(), strtok()
+
+typedef struct movie
+{
+    char title[30];
+    int year; 
+} Movie;
+
+int main(int argc, char *argv[])
+{
+
+
+    return 0;
+}
